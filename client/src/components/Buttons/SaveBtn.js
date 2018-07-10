72574cb8985b46cae5389b8401597b78e777f34e
Leslie Hernandez <lesliehernandez@Leslies-MacBook-Pro.local>
2018-07-10 03:08:07 -0400

    working

--- client/src/components/Buttons/SaveBtn.js
+++ client/src/components/Buttons/SaveBtn.js
@@ -2,6 +2,6 @@ import React from "react";
 import "./Buttons.css";
 
 export const SaveBtn = props =>
-  <button {...props} style={{ float: "right", backgroundColor: "#9C5387", color: "white" }} className="btn saveBtn">
+  <button {...props} style={{ float: "right", backgroundColor: "#404E7C", color: "white" }} className="btn saveBtn">
     {props.children}
   </button>;
